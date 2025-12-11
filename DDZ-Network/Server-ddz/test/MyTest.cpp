024828833080c8e9b348a9541eb8fa5d475fe961
mushan <xiongshunbin@outlook.com>
2025-12-11 22:14:41 +0800

    add: 1.use import to load other proto files;2.use package to define a namespace

--- DDZ-Network/Server-ddz/test/MyTest.cpp
+++ DDZ-Network/Server-ddz/test/MyTest.cpp
@@ -9,7 +9,7 @@
 void MyTest::test()
 {
     // 序列化
-    Person p;
+    Erbing::Person p;
     p.set_id(10);
     p.set_age(32);
     p.set_sex("man");
@@ -19,14 +19,14 @@ void MyTest::test()
     p.add_name("萨博");
     p.mutable_addr()->set_addr("北京市长安街天安门");
     p.mutable_addr()->set_num(1001);
-    p.set_color(Color::Blue);
+    p.set_color(Erbing::Color::Blue);
 
     // 序列化对象p, 最终得到一个字符串
     std::string output;
     p.SerializeToString(&output);
 
     // 反序列化数据
-    Person pp;
+    Erbing::Person pp;
     pp.ParseFromString(output);
     std::cout << pp.id() << ", " << pp.sex() << ", " << pp.age() << std::endl;
     std::cout << pp.addr().addr() << pp.addr().num() << "号" << std::endl;
